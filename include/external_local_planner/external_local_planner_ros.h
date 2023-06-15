34742a584f332bd84e10c0cfd2473679e14bace1
Jarek Karwowski <chromedivizer@gmail.com>
2023-06-16 00:19:42 +0200

    added package config and `ExternalLocalPlannerROS`

---
+++ include/external_local_planner/external_local_planner_ros.h
@@ -0,0 +1,110 @@
+//
+// Author: Jarosław Karwowski,
+// Robot Programming and Machine Perception Group,
+// Warsaw University of Technology
+// 2023
+//
+// Based on DWAPlannerROS implementation by Eitan Marder-Eppstein
+// Ref: https://github.com/ros-planning/navigation
+//
+#pragma once
+
+#include <angles/angles.h>
+#include <base_local_planner/latched_stop_rotate_controller.h>
+#include <base_local_planner/odometry_helper_ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <nav_core/base_local_planner.h>
+#include <nav_msgs/Odometry.h>
+#include <tf2_ros/buffer.h>
+
+#include <mutex>
+
+namespace external_local_planner {
+  /**
+   * @class ExternalLocalPlannerROS
+   * @brief ROS Wrapper for an external planner that adheres to the
+   * BaseLocalPlanner interface and can be used as a plugin for move_base.
+   */
+  class ExternalLocalPlannerROS : public nav_core::BaseLocalPlanner {
+    /// How many times externally received twist command can be applied ('timeout')
+    static constexpr int EXT_CMD_MAX_USAGE_COUNT = 2;
+
+    public:
+      /**
+       * @brief  Constructor for ExternalLocalPlannerROS wrapper
+       */
+      ExternalLocalPlannerROS();
+
+      /**
+       * @brief  Constructs the ros wrapper
+       * @param name The name to give this instance of the trajectory planner
+       * @param tf A pointer to a transform listener
+       * @param costmap The cost map to use for assigning costs to trajectories
+       */
+      void initialize(std::string name, tf2_ros::Buffer* tf,
+          costmap_2d::Costmap2DROS* costmap_ros);
+
+      /**
+       * @brief  Destructor for the wrapper
+       */
+      ~ExternalLocalPlannerROS() = default;
+
+      /**
+       * @brief  Given the current position, orientation, and velocity of the robot,
+       * compute velocity commands to send to the base
+       * @param cmd_vel Will be filled with the velocity command to be passed to the robot base
+       * @return True if a valid trajectory was found, false otherwise
+       */
+      bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+
+      /**
+       * @brief  Set the plan that the controller is following
+       * @param orig_global_plan The plan to pass to the controller
+       * @return True if the plan was updated successfully, false otherwise
+       */
+      bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
+
+      /**
+       * @brief  Check if the goal pose has been achieved
+       * @return True if achieved, false otherwise
+       */
+      bool isGoalReached();
+
+      bool isInitialized() {
+        return initialized_;
+      }
+
+    private:
+      void publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path);
+
+      void externalTwistCb(const geometry_msgs::TwistConstPtr& msg);
+
+      tf2_ros::Buffer* tf_; ///< @brief Used for transforming point clouds
+
+      // publisher of global plan for visualisation
+      ros::Publisher g_plan_pub_;
+
+      // interface with an external local planner
+      ros::Subscriber planner_sub_;
+
+      base_local_planner::LocalPlannerUtil planner_util_;
+
+      costmap_2d::Costmap2DROS* costmap_ros_;
+
+      bool setup_;
+      geometry_msgs::PoseStamped current_pose_;
+
+      base_local_planner::LatchedStopRotateController latched_stop_rotate_controller_;
+
+      bool initialized_;
+
+      base_local_planner::OdometryHelperRos odom_helper_;
+      std::string odom_topic_;
+
+      // Velocity command received from an external module
+      geometry_msgs::Twist cmd_vel_ext_;
+      std::mutex ext_twist_mutex_;
+      // Counter of how many times the externally received command was already applied ("count-out" instead of timeout)
+      int cmd_vel_apply_cnt_;
+  };
+} // namespace external_local_planner
---
+++ src/external_local_planner_ros.cpp
@@ -0,0 +1,154 @@
+//
+// Author: Jarosław Karwowski,
+// Robot Programming and Machine Perception Group,
+// Warsaw University of Technology
+// 2023
+//
+// Based on DWAPlannerROS implementation by Eitan Marder-Eppstein
+// Ref: https://github.com/ros-planning/navigation
+//
+#include <external_local_planner/external_local_planner_ros.h>
+
+#include <Eigen/Core>
+#include <cmath>
+
+#include <base_local_planner/goal_functions.h>
+#include <nav_msgs/Path.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/console.h>
+#include <tf2/utils.h>
+
+//register this planner as a BaseLocalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(external_local_planner::ExternalLocalPlannerROS, nav_core::BaseLocalPlanner)
+
+namespace external_local_planner {
+
+  ExternalLocalPlannerROS::ExternalLocalPlannerROS() : initialized_(false),
+      odom_helper_("odom"), setup_(false), cmd_vel_apply_cnt_(0) {
+  }
+
+  void ExternalLocalPlannerROS::initialize(
+      std::string name,
+      tf2_ros::Buffer* tf,
+      costmap_2d::Costmap2DROS* costmap_ros
+  ) {
+    if (isInitialized()) {
+      ROS_WARN("This planner has already been initialized, doing nothing.");
+    }
+
+    ros::NodeHandle private_nh("~/" + name);
+    g_plan_pub_ = private_nh.advertise<nav_msgs::Path>("global_plan", 1);
+    tf_ = tf;
+    costmap_ros_ = costmap_ros;
+    costmap_ros_->getRobotPose(current_pose_);
+
+    // create an interface for the external planner
+    planner_sub_ = private_nh.subscribe<geometry_msgs::Twist>(
+      "external_cmd_vel",
+      5,
+      &ExternalLocalPlannerROS::externalTwistCb,
+      this
+    );
+
+    // make sure to update the costmap we'll use for this cycle
+    costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
+
+    planner_util_.initialize(tf, costmap, costmap_ros_->getGlobalFrameID());
+
+    if( private_nh.getParam( "odom_topic", odom_topic_ )) {
+      odom_helper_.setOdomTopic( odom_topic_ );
+    }
+
+    initialized_ = true;
+  }
+  
+  bool ExternalLocalPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan) {
+    if (! isInitialized()) {
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    // when we get a new plan, we also want to clear any latch we may have on goal tolerances
+    latched_stop_rotate_controller_.resetLatching();
+
+    ROS_INFO("Got new plan");
+    planner_util_.setPlan(orig_global_plan);
+  }
+
+  bool ExternalLocalPlannerROS::isGoalReached() {
+    if (! isInitialized()) {
+      ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+      return false;
+    }
+    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+      ROS_ERROR("Could not get robot pose");
+      return false;
+    }
+
+    if(latched_stop_rotate_controller_.isGoalReached(&planner_util_, odom_helper_, current_pose_)) {
+      ROS_INFO("Goal reached");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void ExternalLocalPlannerROS::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+    base_local_planner::publishPlan(path, g_plan_pub_);
+  }
+
+  void ExternalLocalPlannerROS::externalTwistCb(const geometry_msgs::TwistConstPtr& msg) {
+    std::lock_guard lock(ext_twist_mutex_);
+    cmd_vel_ext_ = *msg;
+    cmd_vel_apply_cnt_ = 0;
+  }
+
+  bool ExternalLocalPlannerROS::computeVelocityCommands(geometry_msgs::Twist& cmd_vel) {
+    // dispatches to either dwa sampling control or stop and rotate control, depending on whether we have been close enough to goal
+    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+      ROS_ERROR("Could not get robot pose");
+      return false;
+    }
+    std::vector<geometry_msgs::PoseStamped> transformed_plan;
+    if ( ! planner_util_.getLocalPlan(current_pose_, transformed_plan)) {
+      ROS_ERROR("Could not get local plan");
+      return false;
+    }
+
+    //if the global plan passed in is empty... we won't do anything
+    if(transformed_plan.empty()) {
+      ROS_WARN_NAMED("external_local_planner", "Received an empty transformed plan.");
+      return false;
+    }
+    ROS_DEBUG_NAMED("external_local_planner", "Received a transformed plan with %zu points.", transformed_plan.size());
+
+    // evaluate if the external command is valid or outdated
+    bool cmd_valid = cmd_vel_apply_cnt_ <= EXT_CMD_MAX_USAGE_COUNT;
+
+    // increment the counter
+    cmd_vel_apply_cnt_++;
+
+    if (cmd_valid) {
+      publishGlobalPlan(transformed_plan);
+      // send the newest command to the mobile base
+      cmd_vel = cmd_vel_ext_;
+      ROS_DEBUG_NAMED(
+        "external_local_planner",
+        "A valid velocity command of (%.2f, %.2f, %.2f) was found for this cycle.",
+        cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z
+      );
+    } else {
+      std::vector<geometry_msgs::PoseStamped> empty_plan;
+      publishGlobalPlan(empty_plan);
+      // send stop command
+      cmd_vel.linear.x = 0.0;
+      cmd_vel.linear.y = 0.0;
+      cmd_vel.angular.z = 0.0;
+      ROS_WARN_NAMED(
+        "external_local_planner",
+        "External planner failed to produce trajectory in time (%dx). Sending stop command",
+        cmd_vel_apply_cnt_
+      );
+    }
+  }
+
+} // namespace external_local_planner
